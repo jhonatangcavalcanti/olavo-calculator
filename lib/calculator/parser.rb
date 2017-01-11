5ebbad5fb0aed39b471a26e6f9fc8cb45b5a7651
carlos.dias <carlos.dias@corp.globo.com>
2017-01-11 13:24:14 -0200

    Corrigido erros na recursão dos métodos da classe parser que causavam a interpretação incorreta da árvore. Primeira versão funcional do projeto.

--- lib/calculator/parser.rb
+++ lib/calculator/parser.rb
@@ -4,66 +4,98 @@ class Calculator::Parser
 		detect_expression token_array
 	end
 
-	def self.detect_expression token_array
-		# expressões podem ser da forma: (expr), [expr], {expr}, expr (PLUS|MINUS expr)*, term
+	def self.detect_priority_3 token_array
+		# prioridade 3 podem ser da forma: {prior2}, prior2
 		raise "Unexpected Ending" if token_array.empty?
-
-		current = token_array.shift
-		value = 0
 		
-		if [:open_parenthesis, :open_bracket, :open_key].include? current.type
-			value = detect_expression token_array.value
-			raise "Semantic Error" unless current.type == token_array.shift.type
+		if token_array[0].type == :open_key
+			token_array.shift
+			value = detect_priority_2 token_array
+			raise "Semantic Error" if token_array.shift.type != :close_key
+			return value
 		end
 
+		detect_priority_2 token_array
+	end
+
+	def self.detect_priority_2 token_array
+		# prioridade 2 podem ser da forma: [prior1], prior1
+		raise "Unexpected Ending" if token_array.empty?
+		
+		if token_array[0].type == :open_bracket
+			token_array.shift
+			value = detect_priority_1 token_array
+			raise "Semantic Error" if token_array.shift.type != :close_bracket
+			return value
+		end
+
+		detect_priority_1 token_array
+	end
+
+	def self.detect_priority_1 token_array
+		# prioridade 1 podem ser da forma: (expr), expr
+		raise "Unexpected Ending" if token_array.empty?
+		
+		if token_array[0].type == :open_parenthesis
+			token_array.shift
+			value = detect_expression token_array
+			raise "Semantic Error" if token_array.shift.type != :close_parenthesis
+			return value
+		end
+
+		detect_expression token_array
+	end
+
+	def self.detect_expression token_array
+		# expressões podem ser da forma: term (PLUS|MINUS expr)*, term
+		raise "Unexpected Ending" if token_array.empty?
+		value = detect_term token_array
+
 		if token_array.empty?
-			token_array.unshift current
-			return detect_term token_array
+			return value
 		end
 
 		next_token = token_array.shift
 
 		if next_token.type == :plus
-			return detect_term(current) + detect_expression(token_array)
+			return value + detect_expression(token_array)
 		end
 
 		if next_token.type == :minus
-			return detect_term().value - detect_expression(token_array).value
+			return value - detect_expression(token_array)
 		end
 
 		token_array.unshift next_token
-		token_array.unshift current
-
-		return detect_term token_array
+		value
 	end
 
 	def self.detect_term token_array
 		# termos podem ser da forma: factor ((MULT|DIV) factor)*
 		raise "Unexpected Ending" if token_array.empty?
-		current = detect_factor token_array
+		value = detect_factor token_array
 
 		if token_array.empty?
-			return current.value
+			return value
 		end
 		
 		next_token = token_array.shift
 
 		if next_token.type == :mult
-			return current.value * detect_factor(token_array).value
+			return value * detect_factor(token_array)
 		end
 
 		if next_token.type == :div
-			return current.value / detect_factor(token_array).value
+			return value / detect_factor(token_array)
 		end
 
-		raise "Semantic Error"
+		token_array.unshift next_token
+		value
 	end
 
 	def self.detect_factor token_array
-		# fatores podem ser da forma: NUMBER
+		# fatores podem ser da forma: NUMBER, pr3
 		raise "Unexpected Ending" if token_array.empty?
-		current = token_array.shift
-		raise "Semantic Error" unless current.type == :number
-		current
+		return token_array.shift.value if token_array[0].type == :number
+		detect_priority_3 token_array
 	end
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -2,11 +2,15 @@ require "spec_helper"
 
 describe Calculator::Parser do
 	it "detecta um fator" do
-		expect(Calculator::Parser.detect_factor([Calculator::Token.new(:number, 98.34)], 0)).to be 98.34
+		expect(Calculator::Parser.detect_factor([Calculator::Token.new(:number, 98.34)])).to be 98.34
 	end
 
 	it "detecta um termo" do
-		expect(Calculator::Parser.detect_term([Calculator::Token.new(:number, 2), Calculator::Token.new(:plus, :'+'), Calculator::Token.new(:number, 3)])).to eq(5)
+		expect(Calculator::Parser.detect_term([Calculator::Token.new(:number, 2), Calculator::Token.new(:mult, :'*'), Calculator::Token.new(:number, 3)])).to eq 6
+	end
+
+	it "detecta uma expressão" do
+		expect(Calculator::Parser.detect_expression([Calculator::Token.new(:number, 2), Calculator::Token.new(:plus, :'+'), Calculator::Token.new(:number, 3)])).to eq 5
 	end
 
 end
