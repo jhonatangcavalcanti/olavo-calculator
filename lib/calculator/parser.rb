b6fe7d8f393047fad322d0e8c480550cd651d438
carlos.dias <carlos.dias@corp.globo.com>
2017-01-17 11:49:43 -0200

    corrigido erro que ocasionava no cálculo incorreto de expressões que possuiam operadores de prioridade mais a esquerda

--- lib/calculator/parser.rb
+++ lib/calculator/parser.rb
@@ -1,72 +1,46 @@
 class Calculator::Parser
 
+	@@priority_semantics = {
+		:open_parenthesis	=> :close_parenthesis,
+		:open_bracket		=> :close_bracket,
+		:open_key			=> :close_key
+	}
+
 	def self.parse token_array
 		detect_expression token_array
 	end
 
-	def self.detect_priority_3 token_array
-		# prioridade 3 podem ser da forma: {prior2}, prior2
+	def self.detect_priority token_array
+		# prioridades podem ser da forma: (expr), [expr], {expr}, term
 		raise "Unexpected Ending" if token_array.empty?
 		
-		if token_array[0].type == :open_key
-			token_array.shift
-			value = detect_priority_2 token_array
-			raise "Semantic Error" if token_array.shift.type != :close_key
-			return value
-		end
-
-		detect_priority_2 token_array
-	end
-
-	def self.detect_priority_2 token_array
-		# prioridade 2 podem ser da forma: [prior1], prior1
-		raise "Unexpected Ending" if token_array.empty?
-		
-		if token_array[0].type == :open_bracket
-			token_array.shift
-			value = detect_priority_1 token_array
-			raise "Semantic Error" if token_array.shift.type != :close_bracket
-			return value
-		end
-
-		detect_priority_1 token_array
-	end
-
-	def self.detect_priority_1 token_array
-		# prioridade 1 podem ser da forma: (expr), expr
-		raise "Unexpected Ending" if token_array.empty?
-		
-		if token_array[0].type == :open_parenthesis
-			token_array.shift
+		if @@priority_semantics.key? token_array[0].type
+			priority = token_array.shift
 			value = detect_expression token_array
-			raise "Semantic Error" if token_array.shift.type != :close_parenthesis
+			raise "Semantic Error" unless token_array.shift.type == @@priority_semantics[priority.type]
 			return value
 		end
 
-		detect_expression token_array
+		detect_term token_array
 	end
 
 	def self.detect_expression token_array
-		# expressões podem ser da forma: term (PLUS|MINUS expr)*, term
+		# expressões podem ser da forma: priority (PLUS|MINUS expr)*, term
 		raise "Unexpected Ending" if token_array.empty?
-		value = detect_term token_array
+		value = detect_priority token_array
 
-		if token_array.empty?
-			return value
-		end
+		return value if token_array.empty?
 
 		next_token = token_array.shift
 
-		if next_token.type == :plus
-			return value + detect_expression(token_array)
-		end
+		return (value + detect_expression(token_array)) if next_token.type == :plus
 
-		if next_token.type == :minus
-			return value - detect_expression(token_array)
-		end
+		return (value - detect_expression(token_array)) if next_token.type == :minus
 
 		token_array.unshift next_token
-		value
+		token_array.unshift Calculator::Token.new(:number, value)
+
+		detect_term token_array
 	end
 
 	def self.detect_term token_array
@@ -74,28 +48,25 @@ class Calculator::Parser
 		raise "Unexpected Ending" if token_array.empty?
 		value = detect_factor token_array
 
-		if token_array.empty?
-			return value
-		end
+		return value if token_array.empty?
 		
 		next_token = token_array.shift
 
-		if next_token.type == :mult
-			return value * detect_factor(token_array)
+		while next_token && [:mult, :div].include?(next_token.type) do
+			value *= detect_factor(token_array) if next_token.type == :mult
+			value /= detect_factor(token_array) if next_token.type == :div
+			next_token = token_array.shift
 		end
 
-		if next_token.type == :div
-			return value / detect_factor(token_array)
-		end
+		token_array.unshift next_token unless next_token.nil?
 
-		token_array.unshift next_token
 		value
 	end
 
 	def self.detect_factor token_array
-		# fatores podem ser da forma: NUMBER, pr3
+		# fatores podem ser da forma: NUMBER, priority
 		raise "Unexpected Ending" if token_array.empty?
 		return token_array.shift.value if token_array[0].type == :number
-		detect_priority_3 token_array
+		detect_priority token_array
 	end
 end
