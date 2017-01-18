ca713f58f7a58cb7704ab7e38b97b2910a845f97
carlos.dias <carlos.dias@corp.globo.com>
2017-01-18 10:27:00 -0200

    adicionada tarefa padrão

--- lib/calculator/cli.rb
+++ lib/calculator/cli.rb
@@ -3,10 +3,11 @@ require 'calculator'
 
 module Calculator
 	class CLI < Thor
-
 		desc("solve EXPRESSION", "Solves the given EXPRESSION and returns the result")
 		def solve expression
 			puts Calculator.solve expression
 		end
+
+		default_task :solve
 	end
 end
